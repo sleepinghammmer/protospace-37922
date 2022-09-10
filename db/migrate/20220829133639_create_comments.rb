6bfb2a6508887c5d29dc45713c2959918bb66d41
sleepinghammmer <ohagurozawanon@gmail.com>
2022-09-10 20:43:49 +0900

    error 対応

--- db/migrate/20220829133639_create_comments.rb
+++ db/migrate/20220829133639_create_comments.rb
@@ -2,10 +2,10 @@ class CreateComments < ActiveRecord::Migration[4.2]
   def change
     create_table :comments do |t|
       t.text       :text
-      t.bigint :user, limit: 20
-      t.references :prototype, index: true, foreign_key: true
+      t.references :user, limit: 20, type: :bigint, foreign_key: true
+      t.references :prototype, limit: 20, type: :bigint, foreign_key: true
       t.timestamps null: false
     end
-    add_foreign_key :user,:comments 
+    add_foreign_key :user,:prototype,:comments 
   end
 end
