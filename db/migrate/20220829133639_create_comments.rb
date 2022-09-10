aba05b81a6be2401475538239dbf4c7ec9c2ee0d
sleepinghammmer <ohagurozawanon@gmail.com>
2022-09-10 19:28:52 +0900

    StandardError対応

--- db/migrate/20220829133639_create_comments.rb
+++ db/migrate/20220829133639_create_comments.rb
@@ -2,7 +2,7 @@ class CreateComments < ActiveRecord::Migration[4.2]
   def change
     create_table :comments do |t|
       t.text       :text
-      t.references :user, index: true, foreign_key: true
+      t.bigint :user, limit: 20, index: true, foreign_key: true
       t.references :prototype, index: true, foreign_key: true
       t.timestamps null: false
     end
